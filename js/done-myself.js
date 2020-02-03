ebb21c7637b61813a97e7f5698761ab3c3dd7adc
MateoLeon <mateoleon127@gmail.com>
2020-02-02 22:22:45 -0500

    Approved code with eslint, removed contact

--- js/done-myself.js
+++ js/done-myself.js
@@ -1,12 +1,12 @@
-
+//Function created by myself to apply a width depending on its value to the progress bar in skills
 
 (function ($) {
     "use strict";
 
-    $(".bar").each(function(){
+    $(".bar").each(function () {
         $(this).find(".bar-inner").animate({
-          width: $(this).attr("data-width")
-        },2000)
-      });
-    
+            width: $(this).attr("data-width")
+        }, 2000);
+    });
+
 })(jQuery);
--- js/global.js
+++ js/global.js
@@ -1,121 +1,85 @@
 (function ($) {
     "use strict";
-
     // Global variables
-    var html_body = $('html, body');
-
+    let html_body = $("html, body");
     // init Isotope
-    var grid_isotope = $('.isotope').each(function () {
+    let grid_isotope = $(".isotope").each(function () {
         $(this).isotope({
-            itemSelector: '.isotope-item',
+            itemSelector: ".isotope-item",
             percentPosition: true,
-            animationEngine : 'best-available',
+            animationEngine: "best-available",
             masonry: {
-                columnWidth: '.isotope-item'
+                columnWidth: ".isotope-item"
             }
         });
     });
-    var filter = $('.filter-bar');
-    filter.on( 'click', 'li', function() {
-        var filterValue = $(this).attr('data-filter');
-        grid_isotope.isotope({ filter: filterValue });
+    let filter = $(".filter-bar");
+    filter.on("click", "li", function () {
+        let filterValue = $(this).attr("data-filter");
+        grid_isotope.isotope({
+            filter: filterValue
+        });
     });
-    filter.on('click', 'li', function () {
-        filter.find('.active').removeClass('active');
-        $(this).addClass('active');
+    filter.on("click", "li", function () {
+        filter.find(".active").removeClass("active");
+        $(this).addClass("active");
     });
-    $(window).on('load', function () {
+    $(window).on("load", function () {
         grid_isotope.isotope();
     });
 
 
-    // Header
-    var header_pri_fix = $('.header-primary');
-    var header_mob_fix = $('.header-mobile, .header.header-push');
-    var header_fix = $('.header-fixed');
-
-    $(window).on('load', function () {
-        header_pri_fix.after(function () {
-            return "<div class='holder-d' style='height:" + $(this).height() + "px;'></div>";
-        });
-        header_mob_fix.after(function () {
-            return "<div class='holder-m' style='height:" + $(this).height() + "px;'></div>";
-        });
-
-        $(this).on('resize', function () {
-            $('.holder-d').css('height', header_pri_fix.height());
-            $('.holder-m').css('height', header_mob_fix.height());
-        });
-    });
+    let header_fix = $(".header-fixed");
 
 
     // Navbar
     // Navbar vertial and Navbar mobile
-    var handler_slidebar = $('.handler-slidebar');
-    var sliderbar = $('.header-slidebar');
-
-    function toggleSlideBar() {
-        if (sliderbar.hasClass('closed'))
-            sliderbar.addClass('opened').removeClass('closed');
-        else
-            sliderbar.addClass('closed').removeClass('opened');
-    }
+    let sliderbar = $(".header-slidebar");
 
 
-    handler_slidebar.on('click', function () {
-        toggleSlideBar();
-    });
-
-    $(window).on('click', function (e) {
-        if (!$(e.target).closest(sliderbar).length && !$(event.target).closest(handler_slidebar).length) {
-            sliderbar.addClass('closed').removeClass('opened');
-        }
-    });
-
-    $('.navbar-vertical .has-drop .nav-link').on('click', function (e) {
-        $(this).siblings('.drop-menu').slideToggle(200, 'linear');
-        $(this).toggleClass('active');
-        e.preventDefault();
-    });
 
     // Bind to scroll
-    $(window).on('load', function () {
-        var topMenu = $('.header-one-page'),
+    $(window).on("load", function () {
+        let topMenu = $(".header-one-page"),
             menuItems = topMenu.find(".nav-link[href^='#']"),
-            scrollItems = menuItems.map(function() {
-                var item = $($(this).attr("href"));
-                if (item.length) { return item; }
+            scrollItems = menuItems.map(function () {
+                let item = $($(this).attr("href"));
+                if (item.length) {
+                    return item;
+                }
             });
 
-        menuItems.on("click", function (e) {
-            html_body.animate({scrollTop: $($(this).attr("href")).offset().top - header_fix.outerHeight()}, 800).queue(function () {
-                sliderbar.addClass('closed').removeClass('opened');
+        menuItems.on("click", function () {
+            html_body.animate({
+                scrollTop: $($(this).attr("href")).offset().top - header_fix.outerHeight()
+            }, 800).queue(function () {
+                sliderbar.addClass("closed").removeClass("opened");
                 $(this).dequeue();
             });
         });
 
-        $(this).on('scroll', function(){
-            var fromTop = $(this).scrollTop() + topMenu.outerHeight() + 250;
-            var cur = scrollItems.map(function(){
+        $(this).on("scroll", function () {
+            let fromTop = $(this).scrollTop() + topMenu.outerHeight() + 250;
+            let cur = scrollItems.map(function () {
                 if ($(this).offset().top < fromTop)
                     return this;
             });
-            cur = cur[cur.length-1];
-            var id = cur && cur.length ? cur[0].id : "";
+            cur = cur[cur.length - 1];
+            let id = cur && cur.length ? cur[0].id : "";
             menuItems
                 .parent().removeClass("active")
-                .end().filter("[href='#"+id+"']").parent().addClass("active");
+                .end().filter("[href='#" + id + "']").parent().addClass("active");
         });
     });
 
 
 
     // Back To Top
-    var offset = 450;
-    var duration = 500;
-    var upToTop = $("#up-to-top");
+    let offset = 450;
+    let duration = 500;
+    let upToTop = $("#up-to-top");
     upToTop.hide();
-    $(window).on('scroll', function () {
+    $(window).on("scroll", function () {
         if ($(this).scrollTop() > offset) {
             upToTop.fadeIn(duration);
         } else {
@@ -123,37 +87,35 @@
         }
     });
 
-    upToTop.on('click', function (event) {
+    upToTop.on("click", function (event) {
         event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, duration);
+        $("html, body").animate({
+            scrollTop: 0
+        }, duration);
         return false;
     });
 
 
-
-
     // Config Animsition
     $(".animsition").animsition({
-        inClass: 'fade-in',
-        outClass: 'fade-out',
+        inClass: "fade-in",
+        outClass: "fade-out",
         inDuration: 500,
         outDuration: 500,
-        linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])',
+        linkElement: "a:not([target='_blank']):not([href^='#']):not([class^='chosen-single'])",
         loading: true,
-        loadingParentElement: 'html',
-        loadingClass: 'loader-wrapper',
-        loadingInner: '<div class="loader"></div>',
+        loadingParentElement: "html",
+        loadingClass: "loader-wrapper",
+        loadingInner: "<div class='loader'></div>",
         timeout: false,
         timeoutCountdown: 5000,
         onLoadEvent: true,
-        browser: [ 'animation-duration', '-webkit-animation-duration'],
-        overlay : false,
-        overlayClass : 'animsition-overlay-slide',
-        overlayParentElement : 'html',
-        transition: function(url){ window.location.href = url; }
+        browser: ["animation-duration", "-webkit-animation-duration"],
+        overlay: false,
+        overlayClass: "animsition-overlay-slide",
+        overlayParentElement: "html",
+        transition: function (url) {
+            window.location.href = url;
+        }
     });
-
-   
-
-
 })(jQuery);
